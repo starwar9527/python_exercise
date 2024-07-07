cf25c96c8668c7785e51a6e74366509136a707a1
Allen <starwar9527@gmail.com>
2024-07-07 09:27:30 +0200

    add snake repo following Patrick_Loeber

--- tp1.py
+++ tp1.py
@@ -3,7 +3,7 @@ import numpy as np
 from tensorflow import keras
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
+#print(tf.__version__)
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
@@ -18,6 +18,7 @@ data = keras.datasets.fashion_mnist
 #print(len(train_data))
 
 train_data = train_data/256.0
+test_data = test_data/256.0
 
 #print(train_data[7])
 
@@ -29,8 +30,24 @@ for i in range(25):
 	#plt.grid(False)
 	plt.imshow(train_data[i])
 	plt.xlabel(class_names[train_labels[i]])
-plt.show()
+#plt.show()
 
+#build model
+model = tf.keras.models.Sequential([
+	tf.keras.layers.Flatten(input_shape=(28, 28)),
+	tf.keras.layers.Dense(128, activation='relu'),
+	tf.keras.layers.Dropout(0.2),
+	tf.keras.layers.Dense(10)
+])
 
+prediction = model(train_data[:1]).numpy()
+#print(tf.nn.softmax(prediction).numpy())
+loss_fn = tf.keras.losses.sparse_categorical_crossentropy(from_logits=True)
+
+print(loss_fn(train_labels[:1], prediction))
+
+#train_model
+
+#test_model
 
 
