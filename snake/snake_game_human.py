20a9201c996e3070f0b95b1679238ba20b098ea6
Allen <starwar9527@gmail.com>
2024-07-07 11:57:57 +0200

    made some modifications of parameters

--- snake/snake_game_human.py
+++ snake/snake_game_human.py
@@ -19,11 +19,13 @@ Point = namedtuple('Point', 'x, y')
 WHITE = (255, 255, 255)
 RED = (200,0,0)
 BLUE1 = (0, 0, 255)
-BLUE2 = (0, 100, 255)
+#BLUE2 = (0, 100, 255)
+BLUE2 = (0, 200, 0)
 BLACK = (0,0,0)
 
 BLOCK_SIZE = 20
-SPEED = 20
+SPEED = 5
+
 
 class SnakeGame:
     
