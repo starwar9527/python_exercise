ce00385594ddee35de953827194662c5b1488356
Allen <starwar9527@gmail.com>
2024-07-10 17:25:54 +0200

    add agent and model

--- snake/agent.py
+++ snake/agent.py
@@ -1,6 +1,39 @@
 from game import Game
+from model import Model
+from collections import deque
+import os
+
+MAXLEN = 100_000
 
 class Agent:
     def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0
+        self.gamma = 0
+        self.memory = deque(maxlen=MAXLEN)
+        self.game = None
+        self.model = None
+
+    def get_state(self):
         pass
 
+    def get_move(self):
+        pass
+
+    def remember(self):
+        pass
+
+    def train_one_game_step(self):
+        pass
+
+    def train_all_games(self):
+        pass
+
+
+def train():
+    pass
+
+
+if __name__ == '__main__':
+    train()
+    
