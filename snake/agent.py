0a78354bcadccf3d21bc18e4f24611ed831cba68
Allen <starwar9527@gmail.com>
2024-07-10 18:24:02 +0200

    add while loop in Agent::train

--- snake/agent.py
+++ snake/agent.py
@@ -5,12 +5,13 @@ import os
 
 MAXLEN = 100_000
 
+
 class Agent:
     def __init__(self):
         self.n_games = 0
-        self.epsilon = 0
-        self.gamma = 0
-        self.memory = deque(maxlen=MAXLEN)
+        self.epsilon = 0  # randomness
+        self.gamma = 0    # discount rate
+        self.memory = deque(maxlen=MAXLEN)  # popleft() is called when max arrived
         self.model = None
 
     def get_state(self, game):
@@ -19,10 +20,10 @@ class Agent:
     def get_move(self, state):
         pass
 
-    def remember(self, state, reward, score, done):
+    def remember(self, state, action, reward, next_state, done):
         pass
 
-    def train_one_game_step(self, state, reward, score, done):
+    def train_one_game_step(self, state, action, reward, next_state, done):
         pass
 
     def train_all_games(self):
@@ -32,16 +33,18 @@ class Agent:
 def train():
     agent = Agent()
     game = Game()
-    old_state = agent.get_state(game)
-    move = agent.get_move(old_state)
-    reward, score, done = game.play_step(move)
-    new_state = agent.get_state(game)
-    agent.remember(old_state, reward, score, done)
+    while True:
+        old_state = agent.get_state(game)
+        move = agent.get_move(old_state)
+        reward, score, done = game.play_step(move)
+        new_state = agent.get_state(game)
+        agent.remember(old_state, reward, score, done)
 
-    agent.train_one_game_step(old_state, reward, score, done)
-    if done:
-        agent.n_games += 1
-        agent.train_all_games()
+        agent.train_one_game_step(old_state, move, reward, new_state, done)
+        if done:
+            #train all games, plot result
+            agent.n_games += 1
+            agent.train_all_games()
 
 if __name__ == '__main__':
     train()
