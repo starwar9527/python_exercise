4c3553f07118d523c3a94759fd8edefc1b67e25c
Allen <starwar9527@gmail.com>
2024-07-14 15:00:17 +0200

    add test_reset

--- snake/testGame.py
+++ snake/testGame.py
@@ -3,8 +3,17 @@ import unittest
 
 
 class TestGame(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.game = game.Game()
+
     def test_reset(self):
-        self.assertEqual(1, 1)
+        self.game.reset()
+        self.assertEqual(len(self.game.snake), 3)
+        self.assertEqual(self.game.snake[0], self.game.head)
+        self.assertEqual(self.game.score, 0)
+        self.assertEqual(self.game.iteration, 0)
+        self.assertNotEqual(self.game.food, None)
 
 
 if __name__ == '__main__':
