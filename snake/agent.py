cbdf1a2345f967fb390afc5d5feaef27d827aa6e
Allen <starwar9527@gmail.com>
2024-07-14 10:46:25 +0200

    finish imp of agent

--- snake/agent.py
+++ snake/agent.py
@@ -1,4 +1,7 @@
+import random
+
 import numpy as np
+import torch
 
 from game import Game, BLOCK_SIZE, Point, Direction
 from model import Model
@@ -6,6 +9,7 @@ from collections import deque
 import os
 
 MAXLEN = 100_000
+BATCH_SIZE = 1000
 
 
 class Agent:
@@ -15,6 +19,7 @@ class Agent:
         self.gamma = 0    # discount rate
         self.memory = deque(maxlen=MAXLEN)  # popleft() is called when max arrived
         self.model = None
+        self.trainer = None
 
     def get_state(self, game):
         pt = game.head
@@ -62,16 +67,34 @@ class Agent:
         return np.array(state, dtype=int)
 
     def get_move(self, state):
-        pass
+        # random move: tradeoff exploration/exploitation
+        # random move or prediction from model
+        final_move = [0,0,0]
+        if random.randint(0, 200) < 80 - self.n_games:
+            # if the number of games is small, then random
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            # use model for prediction
+            state0 = torch.tensor(state, dtype=float)
+            action = self.model.predict(state0)
+            move = torch.argmax(action).item()
+            final_move[move] = 1
+        return final_move
 
     def remember(self, state, action, reward, next_state, done):
-        pass
+        self.memory.append((state, action, reward, next_state, done))
 
     def train_one_game_step(self, state, action, reward, next_state, done):
-        pass
+        self.trainer.train_one_step(state, action, reward, next_state, done)
 
     def train_all_games(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            batch = random.sample(self.memory, BATCH_SIZE)
+        else:
+            batch = self.memory
+        states, actions, rewards, next_states, dones = zip(*batch)
+        self.trainer.train_one_step(states, actions, rewards, next_states, dones)
 
 
 def train():
