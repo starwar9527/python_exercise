5c963d9e62f9fdea0aa28220f7dadc883e460cf7
Allen <starwar9527@gmail.com>
2024-07-18 21:34:00 +0200

    update model.py

--- snake/model.py
+++ snake/model.py
@@ -1,4 +1,26 @@
+import os.path
 
-class Model:
-    def __init__(self):
-        pass
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Model(nn.Module):
+    def __init__(self, input_layer, hidden_layer, output_layer):
+        super().__init__()
+        self.linear1 = nn.Linear(input_layer, hidden_layer)
+        self.linear2 = nn.Linear(hidden_layer, output_layer)
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
+
+    def save(self, file_name='model.pth'):
+        folder = './Model'
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        full_name = os.path.join(folder, file_name)
+        self.save(self.state_dict(), full_name)
+
+
