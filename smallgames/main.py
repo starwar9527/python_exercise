699be0c3d4ef744c095a0a6f3d1caeeb8056e6ef
Allen <starwar9527@gmail.com>
2024-07-28 21:23:37 +0200

    add main.py for smallgames

---
+++ smallgames/main.py
@@ -0,0 +1,22 @@
+
+while True:
+    id = input('Please select one of the games:\n'
+               '1. Guess\n'
+               '2. StonePaperScissor\n'
+               '3. Straight4\n'
+               '4. Worble\n'
+               '5. TicTacToe\n')
+    if id == '1':
+        print('Guess selected')
+    elif id == '2':
+        print('2 selected')
+    elif id == '3':
+        print('3')
+    elif id == '4':
+        print('4')
+    elif id == '5':
+        print('5')
+    else:
+        print('None of the games is selected, exit')
+        break
+
