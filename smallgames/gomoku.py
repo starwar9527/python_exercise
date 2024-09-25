45c09df648871a79844fae47fdeb87a871606e26
Allen <starwar9527@gmail.com>
2024-09-25 10:16:42 +0200

    update gomoku

--- smallgames/gomoku.py
+++ smallgames/gomoku.py
@@ -16,16 +16,49 @@ class Gomoku:
             row = '  '.join(row)
             print(row)
 
-    def set_piece(self, i, j, c):
+    def assert_valid_position(self, i, j):
         if i < 0 or i > self.row - 1:
             raise ValueError(f"i = {i} is invalid")
         if j < 0 or j > self.column - 1:
             raise ValueError(f"j = {j} is invalid")
+
+    def set_piece(self, i, j, c):
+        self.assert_valid_position(i, j)
         self.pieces[i*self.row+j] = c
 
     def get_piece(self, i, j):
-        if i < 0 or i > self.row - 1:
-            raise ValueError(f"i = {i} is invalid")
-        if j < 0 or j > self.column - 1:
-            raise ValueError(f"j = {j} is invalid")
+        self.assert_valid_position(i, j)
         return self.pieces[i*self.row+j]
+
+    def win(self, last_i, last_j, c):
+        dirs = [[1, 0], [0, 1], [1, 1], [1, -1]]
+        for d in dirs:
+            if self.win_along_dir(d[0], d[1], last_i, last_j, c):
+                return True
+        return False
+
+    def win_along_dir(self, dir_x, dir_y, i, j, c):
+        last_pos = self.get_last_pos_along_dir(dir_x, dir_y, i, j, c)
+        first_pos = self.get_last_pos_along_dir(-dir_x, -dir_y, i, j, c)
+        if last_pos + first_pos >= 4:
+            return True
+        else:
+            return False
+
+    def get_last_pos_along_dir(self, dir_x, dir_y, i, j, c):
+        self.assert_valid_position(i, j)
+        if self.get_piece(i, j) != c:
+            raise ValueError(f"piece at ({i}, {j}) is not {c}")
+
+        pos = 0
+        x = i + dir_x
+        y = j + dir_y
+
+        while 0 <= x < self.row and 0 <= y < self.column and self.get_piece(x, y) == c:
+            pos += 1
+            x += dir_x
+            y += dir_y
+        return pos
+
+    def reset_board(self):
+        self.pieces = [' '] * self.row * self.column
--- smallgames/testGomoku.py
+++ smallgames/testGomoku.py
@@ -32,3 +32,128 @@ class TestGomoku(unittest.TestCase):
 
         self.game.set_piece(2, 1, 'b')
         self.assertEqual('b', self.game.get_piece(2, 1))
+
+    def win_along_dir(self):
+        # check column
+        self.game.set_piece(0, 1, 'X')
+        self.game.set_piece(0, 2, 'X')
+        self.game.set_piece(0, 3, 'X')
+        self.game.set_piece(0, 4, 'X')
+        self.game.set_piece(0, 5, 'X')
+        self.assertTrue(self.game.win_along_dir(0, 1, 0, 1, 'X'))
+        self.assertTrue(self.game.win_along_dir(0, 1, 0, 2, 'X'))
+        self.assertTrue(self.game.win_along_dir(0, 1, 0, 3, 'X'))
+        self.assertTrue(self.game.win_along_dir(0, 1, 0, 4, 'X'))
+        self.assertTrue(self.game.win_along_dir(0, 1, 0, 5, 'X'))
+
+        # test along rows
+        self.game.reset_board()
+
+        self.game.set_piece(1, 0, 'X')
+        self.game.set_piece(2, 0, 'X')
+        self.game.set_piece(3, 0, 'X')
+        self.game.set_piece(4, 0, 'X')
+        self.game.set_piece(5, 0, 'X')
+
+        self.assertTrue(self.game.win_along_dir(1, 0, 1, 0, 'X'))
+        self.assertTrue(self.game.win_along_dir(1, 0, 2, 0, 'X'))
+        self.assertTrue(self.game.win_along_dir(1, 0, 3, 0, 'X'))
+        self.assertTrue(self.game.win_along_dir(1, 0, 4, 0, 'X'))
+        self.assertTrue(self.game.win_along_dir(1, 0, 5, 0, 'X'))
+
+        # test along diagonal
+        self.game.reset_board()
+
+        self.game.set_piece(0, 0, 'X')
+        self.game.set_piece(1, 1, 'X')
+        self.game.set_piece(2, 2, 'X')
+        self.game.set_piece(3, 3, 'X')
+        self.game.set_piece(4, 4, 'X')
+
+        self.assertTrue(self.game.win_along_dir(1, 1, 0, 0, 'X'))
+        self.assertTrue(self.game.win_along_dir(1, 1, 1, 1, 'X'))
+        self.assertTrue(self.game.win_along_dir(1, 1, 2, 2, 'X'))
+        self.assertTrue(self.game.win_along_dir(1, 1, 3, 3, 'X'))
+        self.assertTrue(self.game.win_along_dir(1, 1, 4, 4, 'X'))
+
+        # test along the other diagonal
+        self.game.reset_board()
+
+        self.game.set_piece(0, 4, 'X')
+        self.game.set_piece(1, 3, 'X')
+        self.game.set_piece(2, 2, 'X')
+        self.game.set_piece(3, 1, 'X')
+        self.game.set_piece(4, 0, 'X')
+
+        self.assertTrue(self.game.win_along_dir(1, -1, 0, 4, 'X'))
+        self.assertTrue(self.game.win_along_dir(1, -1, 1, 3, 'X'))
+        self.assertTrue(self.game.win_along_dir(1, -1, 2, 2, 'X'))
+        self.assertTrue(self.game.win_along_dir(1, -1, 3, 1, 'X'))
+        self.assertTrue(self.game.win_along_dir(1, -1, 4, 0, 'X'))
+        
+    def test_win(self):
+        self.game.set_piece(0, 1, 'X')
+        self.game.set_piece(0, 2, 'X')
+        self.game.set_piece(0, 3, 'X')
+        self.game.set_piece(0, 4, 'X')
+        self.game.set_piece(0, 5, 'X')
+        self.assertTrue(self.game.win(0, 5, 'X'))
+        self.assertTrue(self.game.win(0, 2, 'X'))
+
+        self.game.set_piece(0, 5, 'O')
+        self.assertFalse(self.game.win(0, 1, 'X'))
+
+    def test_reset_board(self):
+        self.game.reset_board()
+        for i in range(15):
+            for j in range(15):
+                self.assertEqual(self.game.get_piece(i, j), ' ')
+
+        self.game.set_piece(0, 2, 'X')
+        self.assertEqual(self.game.get_piece(0, 2), 'X')
+
+        self.game.reset_board()
+        self.assertEqual(self.game.get_piece(0, 2), ' ')
+
+    def test_get_last_pos_along_dir(self):
+        # test along columns
+        self.game.reset_board()
+
+        self.game.set_piece(0, 1, 'X')
+        self.game.set_piece(0, 2, 'X')
+        self.game.set_piece(0, 3, 'X')
+        self.game.set_piece(0, 4, 'X')
+        self.game.set_piece(0, 5, 'X')
+
+        pos = self.game.get_last_pos_along_dir(0, 1, 0, 2, 'X')
+        self.assertEqual(pos, 3)
+        pos = self.game.get_last_pos_along_dir(0, -1, 0, 2, 'X')
+        self.assertEqual(pos, 1)
+
+        # test along rows
+        self.game.reset_board()
+
+        self.game.set_piece(1, 0, 'X')
+        self.game.set_piece(2, 0, 'X')
+        self.game.set_piece(3, 0, 'X')
+        self.game.set_piece(4, 0, 'X')
+        self.game.set_piece(5, 0, 'X')
+
+        pos = self.game.get_last_pos_along_dir(1, 0, 2, 0, 'X')
+        self.assertEqual(pos, 3)
+        pos = self.game.get_last_pos_along_dir(-1, 0, 2, 0, 'X')
+        self.assertEqual(pos, 1)
+
+        # test along diagonal
+        self.game.reset_board()
+
+        self.game.set_piece(0, 0, 'X')
+        self.game.set_piece(1, 1, 'X')
+        self.game.set_piece(2, 2, 'X')
+        self.game.set_piece(3, 3, 'X')
+        self.game.set_piece(4, 4, 'X')
+
+        pos = self.game.get_last_pos_along_dir(1, 1, 2, 2, 'X')
+        self.assertEqual(pos, 2)
+        pos = self.game.get_last_pos_along_dir(-1, -1, 3, 3, 'X')
+        self.assertEqual(pos, 3)
